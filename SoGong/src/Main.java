3908a38ae4858ef8246ff1eb77c555d2706f7845
SeongKyeong <SeongKyeong@MinSeongKyeong>
2015-10-31 19:04:09 +0900

    commit

---
+++ SoGong/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.*; 
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		BufferedReader in = new BufferedReader(new InputStreamReader (System.in));
+		
+		wh:while(true){
+			
+			System.out.println();
+			System.out.println("\n");
+			System.out.println("\n");
+			System.out.println("♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬");
+			System.out.println("♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬");
+			System.out.println("♬♬                                                  ♬♬");
+			System.out.println("♬♬        학생  관리 프로그램                       ♬♬");
+			System.out.printlnn("♬♬                                                  ♬♬");
+			System.out.println("♬♬        1)  학생 등록                             ♬♬");
+			System.out.println("♬♬        2)  학생 검색                             ♬♬");
+			System.out.println("♬♬        3)  학생 삭제                             ♬♬");
+			System.out.println("♬♬        4)  학생 기록 수정                        ♬♬");
+			System.out.println("♬♬        5)  프로그램 종료                         ♬♬");
+			System.out.println("♬♬                                                  ♬♬");
+			System.out.println("♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬");
+			System.out.println("♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬♬");
+			System.out.println();
+			System.out.print("메뉴를 선택하세요.(1~5) ");
+
+		}
+	
+
+	}
+}
