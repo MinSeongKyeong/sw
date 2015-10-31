f4a1dcd6fe99a4fbd4d955ac7b862c62427877b4
Hee jin <2zzz_@naver.com>
2015-10-31 19:32:23 +0900

    ViewStudent 일부분 구현

--- SoGong/src/Main.java
+++ SoGong/src/Main.java
@@ -11,24 +11,53 @@ public class Main
 			System.out.println();
 			System.out.println("\n");
 			System.out.println("\n");
-			System.out.println("�ݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢ�");
-			System.out.println("�ݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢ�");
-			System.out.println("�ݢ�                                                  �ݢ�");
-			System.out.println("�ݢ�        �л�  ���� ���α׷�                       �ݢ�");
-			System.out.printlnn("�ݢ�                                                  �ݢ�");
-			System.out.println("�ݢ�        1)  �л� ���                             �ݢ�");
-			System.out.println("�ݢ�        2)  �л� �˻�                             �ݢ�");
-			System.out.println("�ݢ�        3)  �л� ����                             �ݢ�");
-			System.out.println("�ݢ�        4)  �л� ��� ����                        �ݢ�");
-			System.out.println("�ݢ�        5)  ���α׷� ����                         �ݢ�");
-			System.out.println("�ݢ�                                                  �ݢ�");
-			System.out.println("�ݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢ�");
-			System.out.println("�ݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢ�");
+			System.out.println("�ݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢ�");
+			System.out.println("�ݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢ�");
+			System.out.println("�ݢ�                                                  �ݢ�");
+			System.out.println("�ݢ�        �л�  ���� ���α׷�                       �ݢ�");
+			System.out.println("�ݢ�                                                  �ݢ�");
+			System.out.println("�ݢ�        1)  �л� ���                             �ݢ�");
+			System.out.println("�ݢ�        2)  �л� �˻�                             �ݢ�");
+			System.out.println("�ݢ�        3)  �л� ����                             �ݢ�");
+			System.out.println("�ݢ�        4)  �л� ��� ����                        �ݢ�");
+			System.out.println("�ݢ�        5)  ���α׷� ����                         �ݢ�");
+			System.out.println("�ݢ�                                                  �ݢ�");
+			System.out.println("�ݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢ�");
+			System.out.println("�ݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢݢ�");
 			System.out.println();
-			System.out.print("�޴��� �����ϼ���.(1~5) ");
+			System.out.print("�޴��� �����ϼ���.(1~5) ");
 
 		}
+		try{
+			switch(Integer.parseInt(in.readLine())){
+				case 1 : // 학생 등록
+
+
+				case 2 : // 학생 검색
+				
+					ViewStudent view= new ViewStudent(); 
+					view.viewStudent();
+
+					break;
+
+				case 3 : // 학생 삭제
+
+				case 4 : // 학생 정보 수정
+
+				case 5: // 종료
+
+					System.out.print("시스템이 종료 됩니다.  ");
+					break wh;
+
+				default : // 1~5 이외의 것이 입력될 때
+
+					System.out.println();
+					System.out.println("메뉴를 다시 선택하세요");
+					Thread.sleep(1000);
+					continue;
+			} 
+		}
+	}
 	
 
-	}
-}
+
